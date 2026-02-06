987c9b7f3a898a9fb78c1eb94a0f62390d950650
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2026-02-05 22:23:35 -0500

    Fix NPE when file not found, remove optionality of safely -- it is now mandatory.

--- app/src/main/java/io/cwm/engine/resources/ResourceLocator.java
+++ app/src/main/java/io/cwm/engine/resources/ResourceLocator.java
@@ -6,21 +6,21 @@ public class ResourceLocator {
     private String resourceLocation;
 
     public ResourceLocator(String path) {
-        new ResourceLocator(path, false);
-    }
-
-    public ResourceLocator(String path, boolean safely) {
-        if (path.charAt(0) != '/' && safely) {
+        if (path.charAt(0) != '/') {
             path = "/" + path;
         }
         resourceLocation = path;
         if (getResource() == null) {
-            if (!safely) {throw new IllegalArgumentException("Missing file: "+resourceLocation);}
-            else {resourceLocation = null;}
+            resourceLocation = null;
         }
     }
     public String getResourceLocation() {return resourceLocation;}
     public InputStream getResource() {
-        return ResourceLocator.class.getResourceAsStream(resourceLocation);
+        try {
+            return ResourceLocator.class.getResourceAsStream(resourceLocation);
+        }
+        catch (NullPointerException e) {
+            return null; // Implement different handling for different resource types
+        }
     }
 }
